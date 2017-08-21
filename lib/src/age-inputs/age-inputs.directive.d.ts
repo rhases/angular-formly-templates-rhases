eeda16123c4584d5ec0b467cae80d1197106dcfc
Tales Porto <talesap@gmail.com>
2017-08-21 14:20:21 -0300

    ...

--- lib/src/age-inputs/age-inputs.directive.d.ts
+++ lib/src/age-inputs/age-inputs.directive.d.ts
@@ -2,8 +2,9 @@ export default function ageInputsDirective(): {
     template: any;
     scope: {
         model: string;
+        rootModel: string;
         agesOptionsToInclude: string;
         onChange: string;
     };
-    controller: ($scope: any) => void;
+    controller: ($scope: any, $parse: any, $timeout: any) => void;
 };
