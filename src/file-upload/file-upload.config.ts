42675eb8117b18982cb9b9baf6e00b84bc81b7ba
Gabriel Rodrigues <gabrielsr@gmail.com>
2017-10-30 18:42:41 -0200

    melhorias no componente de upload

--- src/file-upload/file-upload.config.ts
+++ src/file-upload/file-upload.config.ts
@@ -6,7 +6,35 @@ export function fileUploadConfig(formlyConfigProvider) {
   formlyConfigProvider.setType({
     name: 'file-upload',
     template: require('./file-upload.pug'),
-    wrapper: ['bootstrapLabel', 'bootstrapHasError']
+    wrapper: ['bootstrapLabel', 'bootstrapHasError'],
+    controller: /* @ngInject */ function ($scope) {
+
+      $scope.opts = $scope.to.options;
+      const opts = $scope.options;
+
+      $scope.uploadStatus = 'none';
+      $scope.fileName = "Escolha um arquivo";
+      $scope.onStarted = function(upload){
+        $scope.fileName = upload.fileName;
+        $scope.uploadStatus = 'processing';
+      }
+
+      $scope.onSuccess= function (upload) {
+        $scope.fileName = upload.fileName;
+        $scope.uploadStatus = 'success';
+      }
+      initInternalModel();
+
+      function initInternalModel() {
+        //init model
+        if ($scope.model[opts.key]) {
+          var str = $scope.model[opts.key];
+          var pieces = str.split(/\//);
+          $scope.fileName = pieces[pieces.length - 1];
+        }
+      }
+    }
   });
+
 
 }
--- src/file-upload/s3-file-upload-directive.directive.ts
+++ src/file-upload/s3-file-upload-directive.directive.ts
@@ -1,3 +1,5 @@
+var Q = require('q');
+
 export function s3FileUploadDirective($http, $q) {
   'ngInject';
   return {
@@ -6,80 +8,148 @@ export function s3FileUploadDirective($http, $q) {
       's3SignRequestUri': '<',
       's3FileNamePrefix': '<',
       's3Bucket': '<',
+      's3Options': '<',
       's3FileUrl': '=',
-      's3UploadStatus': '='
+      's3FilesQueue': '=',
+      's3OnStarted': '&',
+      's3OnSuccess': '&',
+      's3OnProgress': '& ',
     },
-    link: function(scope, el, attrs, ngModelController) {
-      el.bind('change', function(event) {
+    link: function (scope, el, attrs, ngModelController) {
+      el.bind('change', function (event) {
         var files = event.target.files;
-        scope.$apply(function() {
+        scope.$apply(function () {
           ngModelController.$setViewValue(files[0], 'change');
         })
       });
-
-
       scope.ngModelController = ngModelController
     },
-    controller: function($scope, $http, $log, $element) {
+    controller: function ($scope, $http, $log, $element) {
       'ngInject';
 
       var ngModelController = $element.controller('ngModel');
       var uploading;
+      var queue;
 
-      ngModelController.$parsers.push(function(file) {
+      const options = $scope.s3Options || {};
+      options.s3SignRequestUri = $scope.s3SignRequestUri || options.s3SignRequestUri;
+      options.s3FileNamePrefix = $scope.s3FileNamePrefix || options.s3FileNamePrefix;
+      options.s3Bucket = $scope.s3Bucket || options.s3Bucket;
+      if ($scope.s3FilesQueue) {
+        queue = $scope.s3FilesQueue;
+      } else {
+        queue = [];
+      }
+      ngModelController.$parsers.push(function (file) {
         'use strict';
         if (!file || !file.name) { return; }
 
-        $scope.s3UploadStatus = 'processing';
+        var uploadingFile = {
+          url: undefined,
+          fileName: file.name,
+          status: 'processing',
+          size: file.size,
+          type: fileType(file),
+          ext: fileExt(file)
+        };
+
+        queue.push(uploadingFile);
+        if ($scope.s3OnStarted) {
+          $scope.s3OnStarted({ uploadedFile: uploadingFile });
+        }
 
         // Get The PreSigned URL
         uploading = signRequest(file)
-          .then(sendToS3(file))
+          .then(sendToS3(file, uploadingFile))
           .then(function (s3FileUrl) {
-            $scope.s3FileUrl = s3FileUrl
+            $scope.s3FileUrl = s3FileUrl;
+            uploadingFile.url = s3FileUrl;
+            uploadingFile.status = 'success';
+            if ($scope.s3OnSuccess) {
+              $scope.s3OnSuccess({ uploadedFile: uploadingFile });
+            }
             return s3FileUrl;
           })
-          .catch(function(err) {
+          .catch(function (err) {
             $log.error(err);
-            $scope.s3UploadStatus = 'failed';
-            alert("An Error Occurred Attaching Your File");
+            uploadingFile.status = 'failed';
+            alert('An Error Occurred Attaching Your File');
           })
 
         return file;
       });
 
-      ngModelController.$asyncValidators.upload = function(modelValue, viewValue) {
+      ngModelController.$asyncValidators.upload = function (modelValue, viewValue) {
+        if (!uploading) return Q.when();
+
         var value = modelValue || viewValue;
 
-        // Lookup user by username
         return uploading
-          .then(function() {
+          .then(function () {
             return true;
           })
       };
 
       function signRequest(file) {
-        return $http.post($scope.s3SignRequestUri, {
-          filename: $scope.s3FileNamePrefix + file.name,
+        return $http.post(options.s3SignRequestUri, {
+          filename: options.s3FileNamePrefix + file.name,
           type: fileType(file),
-          bucket: $scope.s3Bucket
+          bucket: options.s3Bucket
         })
       }
 
-      function sendToS3(file) {
-        return function(result) {
+      function sendToS3(file, uploadingFile) {
+        return function (result) {
           var preSignedResp = result.data.signedRequest;
+          var onProgress = (x) => { }; //empty function
+          if ($scope.s3OnProgress) {
+            onProgress = $scope.s3OnProgress;
+          }
+
           // Perform The Push To S3
-          return $http.put(preSignedResp, file, { headers: { 'Content-Type': fileType(file) } })
-            .then(function(resp) {
+          return upload(file, preSignedResp, { headers: { 'Content-Type': fileType(file) } })
+            .then(function (response) {
+              console.log('success :) ', response);
+              return response;
+            }, function (err) {
+              console.log('failed :(', err);
+            }, function (e) {
+              console.log('.');
+              onProgress({ event: e, uploadingFile: uploadingFile });
+            })
+            .then(function (resp) {
               $scope.s3UploadStatus = 'success';
               return result.data.url;
             })
         }
       }
 
+      function upload(data, url, headers) {
+        var defer = $q.defer();
+        $http({
+          method: 'PUT',
+          data: data,
+          url: url,
+          headers: headers,
+          uploadEventHandlers: {
+            progress: function (e) {
+              defer.notify(e);
+            }
+          }
+        }).then(defer.resolve.bind(defer), defer.reject.bind(defer));
+        return defer.promise;
+      }
+
       function fileType(file) {
         return file.type || 'application/octet-stream';
+      }
+      function fileExt(file) {
+        if (file.type) {
+          return file.type.split('/')[1];
+        } else if (file.name) {
+          return file.name.split('.')[1];
+        }
+        return '';
       }
     }
   };
