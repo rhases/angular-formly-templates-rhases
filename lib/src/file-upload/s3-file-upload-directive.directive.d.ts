5817f9716517b28a8df7fd0c04837f444f3a6f68
Gabriel Rodrigues <gabrielsr@gmail.com>
2017-10-30 21:33:27 -0200

    build projeto

--- lib/src/file-upload/s3-file-upload-directive.directive.d.ts
+++ lib/src/file-upload/s3-file-upload-directive.directive.d.ts
@@ -4,8 +4,12 @@ export declare function s3FileUploadDirective($http: any, $q: any): {
         's3SignRequestUri': string;
         's3FileNamePrefix': string;
         's3Bucket': string;
+        's3Options': string;
         's3FileUrl': string;
-        's3UploadStatus': string;
+        's3FilesQueue': string;
+        's3OnStarted': string;
+        's3OnSuccess': string;
+        's3OnProgress': string;
     };
     link: (scope: any, el: any, attrs: any, ngModelController: any) => void;
     controller: ($scope: any, $http: any, $log: any, $element: any) => void;
