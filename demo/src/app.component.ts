c3533e15ace80df04ae8a8f36246ea2e5cf41f53
marrcelo <marcelof03@hotmail.com>
2019-08-16 17:13:53 -0300

    improving brazilian-cities adding typeahead in select city

--- demo/src/app.component.ts
+++ demo/src/app.component.ts
@@ -1,205 +1,232 @@
 const angular = require('angular');
 
 export class MainController {
-
   userFields = [
-      {
-          key: 'zipcode',
-          type: 'cep',
-          templateOptions: {
-            label: 'CEP:',
-            required: true
-          },
-      },
-      {
-        key: 'money',
-        type: 'currency',
-        templateOptions: {
-          label: 'Quer pagar quanto:',
-        }
-      },
-      {
-        key: 'cpf',
-        type: 'cpf',
-        templateOptions: {
-          label: 'CPF:',
-          required: true
-        }
-      },
-      {
-        key: 'cnpj',
-        type: 'cnpj',
-        templateOptions: {
-          label: 'CNPJ:',
-        }
-      },
-      {
-        key: 'dateUtc',
-        type: 'utc-date',
-        templateOptions: {
-          label: 'Data UTC:',
-        }
-      },
-      {
-        key: 'billingPeriod',
-        type: 'utc-date',
-        templateOptions: {
-          label: 'Competência:',
-          mask: '99/9999',
-          dateFormat: 'MMYYYY'
-        }
-      },
-      {
-        key: 'date',
-        type: 'date',
-        templateOptions: {
-          label: 'Data:',
-        }
-      },
-      {
-        key: 'to',
-        type: 'big-buttons',
-        templateOptions: {
-          label: 'O plano é para:',
-          options: [
-            { name: 'Você', value: 'you', image: '/assets/images/health-insurance-form/voce.png' },
-            { name: 'Você e sua família', value: 'family', image: '/assets/images/health-insurance-form/familia.png' },
-            { name: 'Empresa', value: 'business', image: '/assets/images/health-insurance-form/empresa.png' },
-            { name: 'Outra pessoa', value: 'another', image: '/assets/images/health-insurance-form/outro.png' },
+    {
+      key: 'location',
+      type: 'brazilian-cities',
+      templateOptions: {
+        shortLabel: 'Local:',
+        required: true
+      }
+    },
+    {
+      key: 'zipcode',
+      type: 'cep',
+      templateOptions: {
+        label: 'CEP:',
+        required: true
+      }
+    },
+    {
+      key: 'money',
+      type: 'currency',
+      templateOptions: {
+        label: 'Quer pagar quanto:'
+      }
+    },
+    {
+      key: 'cpf',
+      type: 'cpf',
+      templateOptions: {
+        label: 'CPF:',
+        required: true
+      }
+    },
+    {
+      key: 'cnpj',
+      type: 'cnpj',
+      templateOptions: {
+        label: 'CNPJ:'
+      }
+    },
+    {
+      key: 'dateUtc',
+      type: 'utc-date',
+      templateOptions: {
+        label: 'Data UTC:'
+      }
+    },
+    {
+      key: 'billingPeriod',
+      type: 'utc-date',
+      templateOptions: {
+        label: 'Competência:',
+        mask: '99/9999',
+        dateFormat: 'MMYYYY'
+      }
+    },
+    {
+      key: 'date',
+      type: 'date',
+      templateOptions: {
+        label: 'Data:'
+      }
+    },
+    {
+      key: 'to',
+      type: 'big-buttons',
+      templateOptions: {
+        label: 'O plano é para:',
+        options: [
+          { name: 'Você', value: 'you', image: '/assets/images/health-insurance-form/voce.png' },
+          { name: 'Você e sua família', value: 'family', image: '/assets/images/health-insurance-form/familia.png' },
+          { name: 'Empresa', value: 'business', image: '/assets/images/health-insurance-form/empresa.png' },
+          { name: 'Outra pessoa', value: 'another', image: '/assets/images/health-insurance-form/outro.png' }
+        ]
+      }
+    },
+    {
+      key: 'contratar',
+      type: 'action-button',
+      templateOptions: {
+        shortLabel: 'Selecionou Contratar:',
+        label: 'Contratar',
+        value: 'sim',
+        required: true
+      }
+    },
+    {
+      key: 'termsOfService',
+      type: 'agree',
+      templateOptions: {
+        label: 'Li e concordo'
+      }
+    },
+    {
+      key: 'bikeGroup',
+      type: 'radio-with-other',
+      templateOptions: {
+        label: 'Grupos:',
+        options: [{ name: 'Não participo.', value: 'no' }],
+        otherLabel: 'Sim, partipo do ',
+        otherPlaceholder: ' ... '
+      }
+    },
+    {
+      key: 'dependents',
+      type: 'multiCheckbox',
+      templateOptions: {
+        label: 'Quem será incluido no plano?',
+        options: [
+          { id: 'partner', title: 'Cônjuge' },
+          { id: 'son', title: 'Filho' },
+          { id: 'father', title: 'Pai' },
+          { id: 'mother', title: 'Mãe' },
+          { id: 'brotherinlaw', title: 'Cunhado' }
+        ],
+        valueProp: 'id',
+        labelProp: 'title'
+      }
+    },
+
+    // Family Ages
+    {
+      key: 'holders',
+      type: 'holder-age-inputs',
+      templateOptions: {
+        label: 'Idades da família:',
+        options: {
+          id: 'holder',
+          label: 'Titular',
+          min: 1,
+          max: 1,
+          default: true,
+          dependents: [
+            { id: 'partner', label: 'Cônjuge', max: 1, default: true, ifIncludedIn: 'scope.dependents' },
+            { id: 'son', label: 'Filho/Enteado', max: 99, default: true, ifIncludedIn: 'scope.dependents' },
+            { id: 'father', label: 'Pai', max: 1, default: true, ifIncludedIn: 'scope.dependents' },
+            { id: 'mother', label: 'Mãe', max: 1, default: true, ifIncludedIn: 'scope.dependents' }
           ]
         }
-      },
-      {
-        key: 'contratar',
-        type: 'action-button',
-        templateOptions: {
-          shortLabel: 'Selecionou Contratar:',
-          label: 'Contratar',
-          value: 'sim',
-          required: true
-        },
-      },
-      {
-        key: 'termsOfService',
-        type: 'agree',
-        templateOptions: {
-          label: 'Li e concordo'
-        }
-      },
-      {
-        key: 'bikeGroup',
-        type: 'radio-with-other',
-        templateOptions: {
-         label: 'Grupos:',
-          options: [
-            { name: 'Não participo.', value: 'no' },
-          ],
-          otherLabel: 'Sim, partipo do ',
-          otherPlaceholder: ' ... '
-        }
-      },
-      {
-        key: 'dependents',
-        type: 'multiCheckbox',
-        templateOptions: {
-          label: 'Quem será incluido no plano?',
-          options: [{ id: 'partner', title: 'Cônjuge' }, { id: 'son', title: 'Filho' }, { id: 'father', title: 'Pai' }, { id: 'mother', title: 'Mãe' }, { id: 'brotherinlaw', title: 'Cunhado' }],
-          valueProp: 'id',
-          labelProp: 'title'
-        }
-      },
-
-      // Family Ages
-      {
-        key: 'holders',
-        type: 'holder-age-inputs',
-        templateOptions: {
-          label: 'Idades da família:',
-          options: { id: 'holder', label: 'Titular', min:1, max: 1, default: true,
-            dependents: [
-              { id: 'partner', label: 'Cônjuge', max: 1, default: true, ifIncludedIn: 'scope.dependents' },
-              { id: 'son', label: 'Filho/Enteado', max: 99, default: true, ifIncludedIn: 'scope.dependents' },
-              { id: 'father', label: 'Pai', max: 1, default: true, ifIncludedIn: 'scope.dependents' },
-              { id: 'mother', label: 'Mãe', max: 1, default: true, ifIncludedIn: 'scope.dependents' }
-          ]}
-        }
-      },
-      {
-        key: 'holders',
-        type: 'holder-age-inputs',
-        templateOptions: {
-          label: 'Idades dos integrantes do plano empresarial:',
-          options: { id: 'employer', label: 'Sócio', groupLabel: 'Sócios', max: 199, default: true,
-            dependents: [
-              { id: 'partner', label: 'Cônjuge', max: 1 },
-              { id: 'son', label: 'Filho/Enteado', max: 99 },
-              { id: 'father', label: 'Pai', max: 1 },
-              { id: 'mother', label: 'Mãe', max: 1 }
-          ]}
-        }
-      },
-      {
-        key: 'fileUrl',
-        type: 'file-upload',
-        templateOptions: {
-          label: 'Arquivo:',
-          bucket: 'rhases-user-insurance-data',
-          fileNamePrefix: 'teste/',
-          signRequestUri:  'http://hi-seller.homolog.api.rhases.com.br/api/s3-upload/preSignedUrlForUserHome',
-          required: false
-        },
-        validation: {
-          messages: {
-            required: '"campo " + to.label + " é obrigatório "'
-          }
-        }
-      },
-      {
-        key: 'attachments',
-        type: 'file-upload',
-        templateOptions: {
-          label: 'Anexos:',
-          bucket: 'rhases-user-insurance-data',
-          fileNamePrefix: 'teste-attachments/',
-          signRequestUri:  'http://hi-seller.homolog.api.rhases.com.br/api/s3-upload/preSignedUrlForUserHome',
-          multipleFiles: true,
-          required: false
-        },
-        validation: {
-          messages: {
-            required: '"campo " + to.label + " é obrigatório "'
-          }
-        }
-      },
-      {
-        key: 'intention',
-        type: 'stack-buttons',
-        templateOptions: {
-          label: 'Intenção:',
-          //label: 'Deseja realizar uma cotaçao ou prefere saber mais o TASSBIKE primeiro?',
-          options: [
-            { name: 'Realizar uma cotação', value: 'quotation', image: 'https://images.rhases.com.br/quote-tassbike/speed.png' },
-            { name: 'Saber mais sobre o Seguro Ciclista', value: 'know-more', image: 'https://images.rhases.com.br/quote-tassbike/mtb.png' }
-          ],
-          required: true,
+      }
+    },
+    {
+      key: 'holders',
+      type: 'holder-age-inputs',
+      templateOptions: {
+        label: 'Idades dos integrantes do plano empresarial:',
+        options: {
+          id: 'employer',
+          label: 'Sócio',
+          groupLabel: 'Sócios',
+          max: 199,
+          default: true,
+          dependents: [
+            { id: 'partner', label: 'Cônjuge', max: 1 },
+            { id: 'son', label: 'Filho/Enteado', max: 99 },
+            { id: 'father', label: 'Pai', max: 1 },
+            { id: 'mother', label: 'Mãe', max: 1 }
+          ]
         }
       }
-    ];
+    },
+    {
+      key: 'fileUrl',
+      type: 'file-upload',
+      templateOptions: {
+        label: 'Arquivo:',
+        bucket: 'rhases-user-insurance-data',
+        fileNamePrefix: 'teste/',
+        signRequestUri: 'http://hi-seller.homolog.api.rhases.com.br/api/s3-upload/preSignedUrlForUserHome',
+        required: false
+      },
+      validation: {
+        messages: {
+          required: '"campo " + to.label + " é obrigatório "'
+        }
+      }
+    },
+    {
+      key: 'attachments',
+      type: 'file-upload',
+      templateOptions: {
+        label: 'Anexos:',
+        bucket: 'rhases-user-insurance-data',
+        fileNamePrefix: 'teste-attachments/',
+        signRequestUri: 'http://hi-seller.homolog.api.rhases.com.br/api/s3-upload/preSignedUrlForUserHome',
+        multipleFiles: true,
+        required: false
+      },
+      validation: {
+        messages: {
+          required: '"campo " + to.label + " é obrigatório "'
+        }
+      }
+    },
+    {
+      key: 'intention',
+      type: 'stack-buttons',
+      templateOptions: {
+        label: 'Intenção:',
+        //label: 'Deseja realizar uma cotaçao ou prefere saber mais o TASSBIKE primeiro?',
+        options: [
+          {
+            name: 'Realizar uma cotação',
+            value: 'quotation',
+            image: 'https://images.rhases.com.br/quote-tassbike/speed.png'
+          },
+          {
+            name: 'Saber mais sobre o Seguro Ciclista',
+            value: 'know-more',
+            image: 'https://images.rhases.com.br/quote-tassbike/mtb.png'
+          }
+        ],
+        required: true
+      }
+    }
+  ];
 
   user = {
-    "dependents": [
-      'partner'
-    ],
-    dateUtc: "1985-10-08T00:00:00.000Z",
-    date: "2018-01-05T03:00:00.000Z",
-    cpf: "642476195",
-    cnpj: "6069286000148"
+    dependents: ['partner'],
+    dateUtc: '1985-10-08T00:00:00.000Z',
+    date: '2018-01-05T03:00:00.000Z',
+    cpf: '642476195',
+    cnpj: '6069286000148'
   };
 
   /*@ngInject*/
-  constructor() {
-
-  }
+  constructor() {}
 
   // $onInit() {
   //   this.$http.get('/api/things').then(response => {
--- src/brazilian-cities/brazilian-cities.config.ts
+++ src/brazilian-cities/brazilian-cities.config.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { getStateByCod, getCityByCod, allStates }  from 'brazilian-cities';
+import { getStateByCod, getCityByCod, allStates } from 'brazilian-cities';
 var _ = require('lodash');
 
 const formly = require('angular-formly');
@@ -10,16 +10,17 @@ export function brazilianCitiesConfig(formlyConfigProvider) {
 
   formlyConfigProvider.setType({
     name: 'brazilian-cities',
+    extends: 'input',
     template: require('./brazilian-cities.pug'),
     wrapper: ['bootstrapLabel', 'bootstrapHasError'],
     defaultOptions: {
-      templateOptions:{ required: true }
+      templateOptions: { required: true }
     },
     controller: /* @ngInject */ function($scope) {
       const to = $scope.to;
       const opts = $scope.options;
 
-      const states =  allStates();
+      const states = allStates();
 
       $scope.set = function(value) {
         $scope.model[opts.key] = value;
@@ -27,55 +28,46 @@ export function brazilianCitiesConfig(formlyConfigProvider) {
         if ($scope.to.onChange) {
           $scope.to.onChange();
         }
-      }
+      };
 
-    	$scope.states = states;
+      $scope.states = states;
 
-    	$scope.onSelectState = function() {
-    		$scope.model[opts.key].city = undefined;
-    	}
+      $scope.onSelectState = function() {
+        $scope.model[opts.key].city = undefined;
+      };
 
-    	$scope.onSelectCity = function() {
-        if(!$scope.internalModel.city)
-          return;
+      $scope.onSelectCity = function() {
+        if (!$scope.internalModel.city) return;
 
         $scope.model[opts.key].state = $scope.internalModel.state.cod;
         $scope.model[opts.key].city = $scope.internalModel.city.cod;
 
+        console.log($scope.model[opts.key].city);
+
         if ($scope.to.onChange) {
           $scope.to.onChange();
         }
-    	}
+      };
 
       initInternalModel();
 
-      function initInternalModel(){
+      function initInternalModel() {
         //init model
-        if(!$scope.model[opts.key]){
+        if (!$scope.model[opts.key]) {
           $scope.model[opts.key] = {};
         }
 
         $scope.internalModel = {};
-    	  var thisModel = $scope.model[opts.key];
-    		if(thisModel.state
-    			&& ( typeof(thisModel.state) === 'string'
-    			|| thisModel.state instanceof String)){
-
-    			$scope.internalModel.state = getStateByCod(thisModel.state)
-    		}
-
-    		if(thisModel.city
-    			&& ( typeof(thisModel.city) === 'string'
-    			|| thisModel.city instanceof String)){
-
-    			$scope.internalModel.city = getCityByCod($scope.internalModel.state, thisModel.city);
-          //$scope.onSelectCity();
+        var thisModel = $scope.model[opts.key];
+        if (thisModel.state && (typeof thisModel.state === 'string' || thisModel.state instanceof String)) {
+          $scope.internalModel.state = getStateByCod(thisModel.state);
         }
 
-    	}
-
-
+        if (thisModel.city && (typeof thisModel.city === 'string' || thisModel.city instanceof String)) {
+          $scope.internalModel.city = getCityByCod($scope.internalModel.state, thisModel.city);
+          //$scope.onSelectCity();
+        }
+      }
     }
   });
-
 }
